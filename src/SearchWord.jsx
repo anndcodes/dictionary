60a205509c374adf93ea21ebb43da886156f5e0d
Annd <ssandreia30@gmail.com>
2024-01-16 15:16:57 -0300

    create functions to handle the word and the search event

--- src/SearchWord.jsx
+++ src/SearchWord.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
+
 function SearchWord() {
+  
+  const [word, setWord] = useState(null)
+
+
+  function handleSearch(event) {
+    event.preventDefault();
+  }
+
+  function handleWord(event) {
+    setWord(event.target.value);
+  }
   
   return (
     <>
       <div className="SearchWord">
         <h1>What word would you like to search?</h1>
-        <form>
-          <input type="search" placeholder="search for a word"/>
+        <form onSubmit={handleSearch}>
+          <input type="search" placeholder="search for a word" onChange={handleWord}/>
         </form>
         <small>ie: code, coffee, computer</small>
       </div>
