90b8438572df06d507a0b902179455352c5d53b4
Annd <ssandreia30@gmail.com>
2024-01-16 17:33:47 -0300

    use map to send meanings results to the new created meaning component

--- src/Word.jsx
+++ src/Word.jsx
@@ -1,12 +1,26 @@
+import Meaning from "./Meaning.jsx";
+
 function Word(props) {
   return (
-    <>
-      <div className="">
-        <h2>{props.result.word}</h2>
-        <h3>{props.result.phonetics[1].text.slice(1, -1)}</h3>
-      </div>
-    </>
-  )
+    <main>
+      <section className="word-phonetic">
+        <h2 className="word">{props.result.word}</h2>
+        <h3 className="phonetic">
+          {props.result.phonetics[1].text.slice(1, -1)}
+        </h3>
+      </section>
+
+      <section className="meanings">
+        {props.result.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default Word;
