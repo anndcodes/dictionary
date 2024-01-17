c601685f20859945eade7cf86272a45d8687aa73
Annd <ssandreia30@gmail.com>
2024-01-17 13:51:47 -0300

    style meaning component (definition title and definition)

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,12 +1,14 @@
+import "./Meaning.css"
+
 function Meaning(props) {
   return (
     <>
       {props.meaning.definitions.map(function (definition, index) {
         if (index <= 2) {
           return (
-            <div key={index}>
-              <h3>{props.meaning.partOfSpeech}</h3>
-              {definition.definition}
+            <div key={index} className="definition-box">
+              <h3 className="definition-title">{props.meaning.partOfSpeech}</h3>
+              <p className="definition">{definition.definition}</p>
             </div>
           );
         } else {
