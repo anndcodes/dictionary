360c65dddcf751744ce1e5e6b397b09338835cd4
Annd <ssandreia30@gmail.com>
2024-01-17 16:01:41 -0300

    fix bug of not receiving results from the api because some word's arrays do not match

--- src/Word.jsx
+++ src/Word.jsx
@@ -1,27 +1,50 @@
 import Meaning from "./Meaning.jsx";
-import "./Word.css"
+import "./Word.css";
 
 function Word(props) {
-  return (
-    <>
-      <section className="word-phonetic">
-        <h2 className="word">{props.result.word}</h2>
-        <h3 className="phonetic">
-          {props.result.phonetics[1].text.slice(1, -1)}
-        </h3>
-      </section>
+  if (props.result.phonetics[1]) {
+    return (
+      <>
+        <section className="word-phonetic">
+          <h2 className="word">{props.result.word}</h2>
+          <h3 className="phonetic">
+            {props.result.phonetics[1].text.slice(1, -1)}
+          </h3>
+        </section>
 
-      <section className="meanings">
-        {props.result.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
-      </section>
-    </>
-  );
+        <section className="meanings">
+          {props.result.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </section>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <section className="word-phonetic">
+          <h2 className="word">{props.result.word}</h2>
+          <h3 className="phonetic">
+            {props.result.phonetics[0].text.slice(1, -1)}
+          </h3>
+        </section>
+
+        <section className="meanings">
+          {props.result.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </section>
+      </>
+    );
+  }
 }
 
 export default Word;
